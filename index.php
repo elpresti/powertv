12730aeefddd2200009ec94a450e4c2dc06e97fe
Sebastian Prestifilippo <elpresti@gmail.com>
2013-12-30 00:00:50 +0000

    13

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <html>
   <head>
      <title>Radio Power TV</title>
+      <meta name="description" content="PowerTV - Desde nuestro sitio podes miranos en HD las 24hs, videoclips, clima, móviles, bosque+playa=Pinamar!">
+      <meta name="keywords" content="PowerTV,Radio Power Pinamar,Videoclips online,Musica en vivo, musica, Radio en vivo, Radio HD, Pinamar">
+      <meta property="og:title" content="Radio Power Pinamar - PowerTV">
+      <meta property="og:type" content="website">
+      <meta property="og:url" content="http://www.powerhd.com.ar/">
+      <meta property="og:image" content="http://radiopower.com.ar/images/facebookShareLogo.png">
+      <meta property="og:description" content="Desde Pinamar y para todo el mundo podes vernos en HD y escucharnos las 24hs a traves de nuestro sitio. No te lo pierdas!">
+      <meta property="og:site_name" content="PowerTV">
   </head>
   <body>
      <?php 
         echo '<p>Redireccionando al reproductor de PowerTV</p>';
         if (strlen($_SERVER['QUERY_STRING'])>0){
-          header("Location: webroot/index.php?".$_SERVER['QUERY_STRING']);
+          header("Location: webroot/index.html?".$_SERVER['QUERY_STRING']);
         }else{
-          header("Location: webroot/index.php");
+          header("Location: webroot/index.html");
         }
      ?>
   </body>
