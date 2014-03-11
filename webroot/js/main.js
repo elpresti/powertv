70a1f82787be4edc2367f218e56fa325c7ef737b
Sebastian Prestifilippo <elpresti@gmail.com>
2014-03-11 16:52:58 +0000

    Reconnect changes and Facebook Wall plugin change

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -1,6 +1,12 @@
 var fbFlipped=false;
 var deviceOs="desktop";
 var deviceType="desktop";
+var connectionErrorShown=false;
+var reconnectAttempts=0;
+var connectingErrorMsg="noErrorsYet";
+var urlReconnect="/?utm_source=webapp&utm_medium=autoReconnect&utm_campaign=powertv&connectingErrorMsg="+connectingErrorMsg;
+var connectRetry = null;
+
 
 function initScripts(){
   setAnimationVars();
@@ -15,6 +21,9 @@ function initScripts(){
     }
   });
   
+  //PlusGallery init
+  $('#plusgallery').plusGallery();
+  
   //YoutubeTV Player init
   $(window).load(function(){
 				$('#youtubePwrPlayer').ytv({
@@ -25,10 +34,7 @@ function initScripts(){
           chainVideos: true
 				}); 
 	});
-  
-  //PlusGallery init
-  $('#plusgallery').plusGallery();
-  
+   
 }
 
 function autoplayMobileStream(){
@@ -57,7 +63,7 @@ function setAnimationVars(){
   $(function(){
     $('#pwrFbWall').fbWall({ 
         id:'radiopowerpinamar',
-        accessToken:'CAAFF4Xs8Wd0BAEvuudcutY4WLauzbOQZBTRxZAnsyNE99lAadYTyETvIOOPfDHzUtqbZAg0BXrNYodBS6rLrrhP74lRfmGNMNTFx7SxChVEQTnytTRpyZArrPZCNDQWqPyldQvzj9Si3FzeMMG4oZAFWlKAgeydZBY0USZAAb4cZCAsJaGFdP6ZAzSTdcKhSmidK4ZD',
+        accessToken:'CAAFF4Xs8Wd0BAKXXVN3vE9ymZAfjqVlF2cUK0HShZBVF2SngkZA3Ypm8nIlGJkOnNcXF13zB2wYqZCjZAxrMcvBGkEN0mpNJLZAcc12t7vt8hNGZAiEUH3Km6ctkZCiAdZCwha8DDuGopvUd3dZCLZAsh41gpPhhiCJXK4i8XhXYzrifIyX18LLuc80',
         showGuestEntries: true,
         showComments: true,
         max: 10,
@@ -73,6 +79,7 @@ function setAnimationVars(){
   };
   
   //animateElement("bounceIn","headerContainer");//animate logo
+  //personalizeFbWall();
 }
 
 function flipFacebookBox(fxIn,fxOut){
@@ -125,12 +132,114 @@ function addVideoPlayer(deviceType, deviceOS) {
                 divVideoPlayer += "<area shape='rect' coords='423,45,598,351' href='https://itunes.apple.com/es/app/vlc-for-ios/id650377962' target='_blank'></map></div>";
             }
         }
+        $("#dynamicCodeOfVideoPlayer").append(divVideoPlayer);
         autoplayMobileStream();
+    }else {
+      checkBrowserFlashReady();
     }
-    $("#dynamicCodeOfVideoPlayer").append(divVideoPlayer);
 }
 
-function doActionsWhileErrorConnecting(){
-  alert("Error al intentar reproducir nuestra transmisión en vivo, te invitamos a revivir grabaciones de Momentos Power en el reproductor que está al pié de esta página e intentar nuevamente en unos minutos");
+function addNoFlashMsg(){
+   $('#videoPlayer').css('display','none');
+   divVideoPlayer = "<div id='noFlashMsgContainer' class='noFlashMsgContainer'>";
+   divVideoPlayer += "<a target='_blank' href='http://get.adobe.com/flashplayer/'><img src='img/flashPlayerLogo.png' alt='Click aquí para descargar Flash Player' width='147' height='144' border='0' /></a>";
+   divVideoPlayer += "<p>Se necesita Adobe Flash Player para ver PowerHD.<br> <a target='_blank' href='http://get.adobe.com/flashplayer/'>Obtén la última versión de Flash Player</a></p></div>";
+   $("#dynamicCodeOfVideoPlayer").append(divVideoPlayer); 
 }
 
+
+function doActionsWhileErrorConnecting(message){
+  if (message !== undefined ){//onError
+    console.log('Video Player Error Message >>> '+message);
+    connectingErrorMsg=message;
+  }
+  reconnectVideoPlayer();
+/*
+  if (reconnectAttempts===0){
+    connectRetry = setInterval(reconnectVideoPlayer, 16000);
+  }else {
+    reconnectVideoPlayer();
+  }
+*/
+}
+
+function isBrowserFlashEnabled(){
+  var errorShown=false;
+  var errorMsgTag = document.getElementById('videoPlayer');
+  if(errorMsgTag !== undefined && errorMsgTag.getElementsByTagName("p") !== undefined && errorMsgTag.getElementsByTagName("p").length > 0){
+    errorMsgTag = errorMsgTag.getElementsByTagName("p")[0].innerHTML.toLowerCase();
+    if ( (errorMsgTag.indexOf('no playable sources found') != -1) || 
+         ( (errorMsgTag.indexOf('error') != -1) && (errorMsgTag.indexOf('flash') != -1) )
+      ){
+        errorShown=true;
+        console.log('Video Player Error Message >>> '+errorMsgTag);
+        connectingErrorMsg=errorMsgTag;
+        //getUrlReconnect();
+    }
+  }
+  return (!(errorShown)); 
+}
+
+function checkBrowserFlashReady(){
+  if (!(isBrowserFlashEnabled())){
+      //console.log('Flash Player NO está instalado o está deshabilitado');
+      alert('Flash Player no está instalado o está deshabilitado o su versión instalada es obsoleta');
+      addNoFlashMsg();
+    }else {
+      //console.log('Flash Player SI está instalado y habilitado');
+    }
+}
+
+function reconnectVideoPlayer(){
+  if (reconnectAttempts<=3){
+    reconnectAttempts=reconnectAttempts+1;
+    console.log('Cantidad de intentos de reconexión: '+reconnectAttempts);
+    jwplayer().play(true);
+  }else{
+      if (!connectionErrorShown){
+        showConnectionProblemsMsg();
+        connectionErrorShown=true;
+      }
+  }
+}
+
+function showConnectionProblemsMsg(){
+   $('#videoPlayer').css('display','none');
+   $('#videoPlayer_wrapper').css('display','none');
+   divVideoPlayer = "<div id='connectionProblemsMsgContainer' class='connectionProblemsMsgContainer'>";
+   divVideoPlayer += "<a href='"+getUrlReconnect()+"'><img src='img/imgConnectionProblems.png' alt='Click aquí para recargar la página ahora' width='120' height='119' border='0' /></a>";
+   divVideoPlayer += "<p>No se pudo conectar al servidor de PowerHD.<br> <a href='"+getUrlReconnect()+"'>Vuelve a intentarlo en unos minutos</a> ó utiliza nuestro <a target='_blank' href='http://www.radiopower.com.ar/audioplayer/index.php?alternativo=1&utm_source=webapp&utm_medium=errorConnect&utm_campaign=powertv&connectingErrorMsg="+connectingErrorMsg+"'>reproductor de audio</a>.<br><a href='#youtubeWidgetContainer'>Ver Grabaciones</a></p></div>";
+   $("#dynamicCodeOfVideoPlayer").append(divVideoPlayer);
+   alert("Error al intentar reproducir nuestra transmisión en vivo, te invitamos a revivir grabaciones de Momentos Power en el reproductor que está al pié de esta página e intentar nuevamente en unos minutos");
+}
+
+function getUrlReconnect(){
+  urlReconnect="/?utm_source=webapp&utm_medium=autoReconnect&utm_campaign=powertv&connectingErrorMsg="+connectingErrorMsg;
+  return urlReconnect;
+}
+
+function showPlayerStatus(){
+  console.log(jwplayer().getState());
+} 
+
+function doActionsOnPlay(){
+}
+
+function personalizeFbWall(){
+  //var element = document.getElementById('my-fb-like-box').getElementsByTagName("iframe")[0];
+  //document.getElementById('my-fb-like-box').getElementsByTagName("iframe")[0].style.height = '430px';
+  $('#my-fb-like-box').children().children().css('height',$('#my-fb-like-box').children().children().height()-20);
+  //element.style.height = element.style.height - '20px';
+  //element = element.getElementsByTagName("span"); //element.$('span').get();
+  //element = element.getElementsByTagName("iframe"); //element.$('iframe').get();
+  //element.style.height = element.style.height-20; //$(element).css('height',element.style.height-20);
+  console.log("Se ejecuto la personalización del Muro Power");
+}
+
+
+
+$(document).ready(function(){
+  //console.log(videoPlayerVar.getState());
+  personalizeFbWall();
+  }
+)
